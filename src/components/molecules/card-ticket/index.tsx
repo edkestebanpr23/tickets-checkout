4c3060801fb55c25cc2265b11fac4708a451019a
Esteban Parra <edkestebanpr23@gmail.com>
2022-06-19 14:57:28 -0500

    feat: Created initial mockdata and Card tickets component

---
+++ src/components/molecules/card-ticket/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Button, Grid, Typography } from '@mui/material';
+import './styles.scss';
+
+type Props = {
+  ticket: {
+    event: string;
+    place: string;
+    location: string;
+    hour: string;
+    date: string;
+    id: string;
+  },
+  categorie: string;
+  subcategory: string;
+};
+
+const CardTicket = ({ ticket, categorie, subcategory }: Props): JSX.Element => {
+  console.log(1);
+  return (
+    <Grid container>
+      <Grid item xs={12} className="Card">
+        <Grid container>
+          <Grid item xs={4} className="image" />
+          <Grid item xs={5} className="body">
+            <Typography variant="h5" className="title">
+              {ticket.event}
+            </Typography>
+            <Grid className="location">
+              <Typography variant="h6" className="place">
+              {ticket.place}
+              </Typography>
+              <Typography variant="body1" className="country">
+              {ticket.location}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={3} className="body">
+            <Typography variant="body1" className="hour">
+            {ticket.hour}
+            </Typography>
+            <Typography variant="body1" className="date">
+            {ticket.date}
+            </Typography>
+            <Grid className="btn-ctn">
+              <Button variant="contained">Buy tickets</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CardTicket;
