10ada7f22e25fe5b4a118adc0e7ec246c70c4f1c
Esteban Parra <edkestebanpr23@gmail.com>
2022-06-19 00:49:13 -0500

    feat: Left side, List were created

--- src/components/atoms/body/index.tsx
+++ src/components/atoms/body/index.tsx
@@ -1,28 +1,34 @@
 import * as React from 'react';
-import { Grid, makeStyles } from '@mui/material';
+import { Grid, makeStyles, Typography } from '@mui/material';
 import './styles.scss';
+import ListCategories from '../../organism/list-categories';
 
 type Props = {
-    children: JSX.Element
-}
+  children: JSX.Element;
+};
 
 const Body = ({ children }: Props): JSX.Element => {
   console.log(1);
   return (
-        <Grid
-          container
-          className="Body"
-        >
-            <Grid container className="container">
-                <Grid item xs={4} className="container-left">
-                    {/* {children} */}
-                </Grid>
-                <Grid item xs={8} className="container-rigth">
-                    {/* {children} */}
-                </Grid>
-
-            </Grid>
+    <Grid container className="Body">
+      <Grid container className="container">
+        <Grid item xs={4} className="container-left">
+          <ListCategories />
         </Grid>
+        <Grid item xs={8} className="container-rigth">
+          <Grid container>
+            <Grid item xs={12} className="Card">
+              <Grid container>
+                <Grid item xs={4} className="image" />
+                <Grid item xs={8} className="body">
+                    <Typography>Shakira</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
---
+++ src/components/organism/list-categories/index.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import './styles.scss';
+import ListGroup from '../../molecules/list-group';
+
+import { config } from '../../../config';
+
+const {
+  mockdata: { categories }
+} = config;
+
+const ListCategories = (): JSX.Element => {
+  console.log(categories);
+  const [open, setOpen] = React.useState<boolean>(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      sx={{
+        width: '100%',
+        bgcolor: 'primary.main',
+        color: 'primary.contrastText'
+      }}
+      className="ListCategories"
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={(
+        <ListSubheader
+          component="div"
+          id="nested-list-subheader"
+          className="header"
+          sx={{
+            width: '100%',
+            bgcolor: 'primary.main',
+            color: 'primary.contrastText'
+          }}
+        >
+          Categories
+        </ListSubheader>
+      )}
+    >
+      {categories.map((category) => (
+        <ListGroup category={category} key={category.category} />
+      ))}
+    </List>
+  );
+};
+
+export default ListCategories;
