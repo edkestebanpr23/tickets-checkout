c5f2a2380426eb35d5affe8072c7a84d88596ff5
Esteban Parra <edkestebanpr23@gmail.com>
2022-06-19 21:26:10 -0500

    feat: Added React Redux Toolkit

--- src/components/atoms/list-item/index.tsx
+++ src/components/atoms/list-item/index.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setCategory,
+  setSubcategory,
+  State
+} from '../../../features/ticket/ticketSlice';
+
 import './styles.scss';
 
 interface ListGroupProps {
@@ -10,10 +17,25 @@ interface ListGroupProps {
   };
 }
 
-const ListItem = ({ category }: ListGroupProps): JSX.Element => (
-  <ListItemButton sx={{ pl: 8, bgcolor: 'secondary.main' }}>
-        <ListItemText primary={category.category} />
-  </ListItemButton>
-);
+const ListItem = ({ category }: ListGroupProps): JSX.Element => {
+  const category2 = useSelector((state: any) => state.tickets.category);
+  const subcategory = useSelector((state: any) => state.tickets.subcategory);
+
+  const dispatch = useDispatch();
+
+  const onHandleClick = () => {
+    dispatch(setSubcategory(category.category));
+  };
+
+  console.log('res', category2, subcategory);
+  return (
+    <ListItemButton
+      sx={{ pl: 8, bgcolor: 'secondary.main' }}
+      onClick={onHandleClick}
+    >
+      <ListItemText primary={category.category} />
+    </ListItemButton>
+  );
+};
 
 export default ListItem;
