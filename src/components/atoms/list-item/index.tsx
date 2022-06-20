81b80c7559ff36b51ca65cbc72252d63d443d246
Esteban Parra <edkestebanpr23@gmail.com>
2022-06-19 23:07:01 -0500

    feat: Connected cards & menu with redux

--- src/components/atoms/list-item/index.tsx
+++ src/components/atoms/list-item/index.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {
   setCategory,
-  setSubcategory,
-  State
+  setSubcategory
 } from '../../../features/ticket/ticketSlice';
 
 import './styles.scss';
@@ -14,26 +13,24 @@ import './styles.scss';
 interface ListGroupProps {
   category: {
     category: string;
+    subcategory: string;
   };
 }
 
 const ListItem = ({ category }: ListGroupProps): JSX.Element => {
-  const category2 = useSelector((state: any) => state.tickets.category);
-  const subcategory = useSelector((state: any) => state.tickets.subcategory);
-
   const dispatch = useDispatch();
 
   const onHandleClick = () => {
-    dispatch(setSubcategory(category.category));
+    dispatch(setSubcategory(category.subcategory));
+    dispatch(setCategory(category.category));
   };
 
-  console.log('res', category2, subcategory);
   return (
     <ListItemButton
       sx={{ pl: 8, bgcolor: 'secondary.main' }}
       onClick={onHandleClick}
     >
-      <ListItemText primary={category.category} />
+      <ListItemText primary={category.subcategory} />
     </ListItemButton>
   );
 };
